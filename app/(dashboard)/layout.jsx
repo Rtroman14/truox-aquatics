35f86d1c1348b358f4776516db035ecb067a1933
Ryan Roman <rtroman14@gmail.com>
2024-05-30 13:44:11 -0600

    remove header

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -136,9 +136,9 @@ export default function Layout({ children }) {
                     </DropdownMenu>
                 </header>
                 <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                    <div className="flex items-center">
+                    {/* <div className="flex items-center">
                         <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-                    </div>
+                    </div> */}
                     <div
                         className="items-center justify-center rounded-lg border border-dashed shadow-sm dark:bg-dot-white/[0.2] bg-dot-black/[0.2]"
                         x-chunk="dashboard-02-chunk-1"
