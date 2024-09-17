cae89cdfd44e766e7749ba8d3f3366813fbc3046
Ryan Roman <rtroman14@gmail.com>
2024-09-17 08:41:01 -0600

    fix path

--- app/(marketing)/reset-password/_components/ResetPasswordForm.jsx
+++ app/(marketing)/reset-password/_components/ResetPasswordForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "@/app/lib/supabase/client";
+import { createClient } from "@/lib/supabase/client";
 import { Loader2 } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
